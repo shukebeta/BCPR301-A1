90cc3669f442d2488e12573ffb0407f735515cd1
shukebeta <weizhong2004@gmail.com>
2019-09-21 10:52:14 +1200

    fix Speculative Generality bad smell: unused method

--- tigr/drawer/tkinter_worker.py
+++ tigr/drawer/tkinter_worker.py
@@ -84,11 +84,9 @@ class TkinterWorker(tk.Tk):
 
     def penup(self):
         self._pendown = False
-        self.debug()
 
     def pendown(self):
         self._pendown = True
-        self.debug()
 
     def pensize(self, size):
         self._pensize = int(size)
@@ -99,7 +97,6 @@ class TkinterWorker(tk.Tk):
         else:
             self._heading = 180
         self.forward(abs(length))
-        self.debug()
 
     def forward(self, length):
         x, y = self._calc_target_pos(self._heading, length)
@@ -109,14 +106,12 @@ class TkinterWorker(tk.Tk):
             self.goto(x, y)
 
         self.update()
-        self.debug()
 
     def goto(self, x, y):
         self.pos['x'] = x
         self.pos['y'] = y
 
         self.update()
-        self.debug()
 
     def go_along(self, along):
         if along > 0:
@@ -136,10 +131,6 @@ class TkinterWorker(tk.Tk):
     def pencolor(self, pencolor):
         self._pencolor = pencolor
 
-    def debug(self):
-        # print(self.pos)
-        pass
-
     def update(self):
         super().update()
         time.sleep(self.wait)
@@ -156,13 +147,11 @@ class TkinterWorker(tk.Tk):
         else:
             self._draw_line(x, y)
 
-
     def _draw_line(self, x, y):
         id = self.canvas.create_line(self.pos['x'], self.pos['y'], x, y, fill=self._pencolor, width=self._pensize)
         self.draw_history.append(id)
         self.goto(x, y)
         self.update()
-        self.debug()
 
     def _calc_target_pos(self, direction, length):
         return (self.pos['x'] + math.sin(math.radians(direction)) * length,
